a0255938fd577e8d2baa83cc10e1f4391b6983bc
Jurgen Mikk <jurgen.mikk@khk.ee>
2021-12-03 10:56:39 +0200

    OOP get weather data

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-//
+const weather = new Weather('Tallinn')
+
+// get City weather
+
+function getWeather(){
+    weather.getWeather()
+        .then(data => {
+
+            console.log(data)
+
+        })
+        .catch(error =>{
+
+           console.log(error)
+
+        })
+}
+
+
+getWeather()
--- classes/Weather.js
+++ classes/Weather.js
@@ -1,1 +1,14 @@
-//
+class Weather {
+    constructor(city){
+        this.city = city
+        this.key = '9521375636ffb7a426265026b4ac913c';
+    }
+    //
+   async getWeather(){
+        const response = await fetch((`http://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${this.key}`))
+       const responseData = await response.json()
+       //tagastab responseData - saab kasutusele votta
+       return responseData
+    }
+
+}
